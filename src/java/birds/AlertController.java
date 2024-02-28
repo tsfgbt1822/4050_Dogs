efba3b11a8907a44dcc95a7c4691f3ee6b60964b
Tori Fischer <145940832+tsfgbt1822@users.noreply.github.com>
2024-02-28 09:52:23 -0600

    Update AlertController.java
    changed package to dogs

--- src/java/birds/AlertController.java
+++ src/java/birds/AlertController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package assignment.birds;
+package assignment.dogs;
 
 import java.net.URL;
 import java.util.ResourceBundle;
