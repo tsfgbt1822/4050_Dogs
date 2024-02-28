a7091a42e7824a0513d6d7d039f0ca90118b0b58
Tori Fischer <145940832+tsfgbt1822@users.noreply.github.com>
2024-02-28 09:53:13 -0600

    Update and rename BirdRecord.java to DogRecord.java
    Updated all birds to dogs

--- src/java/birds/DogRecord.java
+++ src/java/birds/DogRecord.java
@@ -1,10 +1,10 @@
-package assignment.birds;
+package assignment.dogs;
 
 /**
  * This class represents a bird record in the database. Each record consists of two
  * parts: a DataKey and the data associated with the DataKey.
  */
-public class BirdRecord {
+public class DogRecord {
 
     private DataKey key;
     private String about;
@@ -12,11 +12,11 @@ public class BirdRecord {
     private String image;
 
     // default constructor
-    public BirdRecord() {
+    public DogRecord() {
         this(null, null, null, null);
     }
 
-    public BirdRecord(DataKey k, String a, String s, String i) {
+    public DogRecord(DataKey k, String a, String s, String i) {
         key = k;
         about = a;
         sound = s;
